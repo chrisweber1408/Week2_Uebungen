c5cddbd382577461764a7b24d40145d17a9fbf78
chris <chrisweber1991@gmail.com>
2022-05-23 13:17:50 +0200

    Klassen erste Aufgabe

---
+++ src/main/java/Student.java
@@ -0,0 +1,31 @@
+public class Student {
+
+        private String name = "Name";
+        private int age = 0;
+        private String gender = "Geschlecht";
+
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+
+        public int getAge(){
+            return age;
+        }
+        public void setAge(int age){
+            this.age = age;
+        }
+
+        public String getGender(){
+            return gender;
+        }
+        public void setGender(String gender){
+            this.gender = gender;
+        }
+
+
+}
+
---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StudentTest {
+
+    @Test
+    void ShouldBeTrueSetName() {
+        //Given
+        Student student1 = new Student();
+        //When
+        student1.setName("Christoph Weber");
+        String student1Name = student1.getName();
+        //Then
+        Assertions.assertEquals("Christoph Weber", student1Name);
+    }
+
+    @Test
+    void ShouldBeTrueSetAge(){
+        Student student1 = new Student();
+        student1.setAge(30);
+        int studentAge = student1.getAge();
+        Assertions.assertEquals(30,studentAge);
+    }
+
+    @Test
+    void ShouldBeTrueSetGender(){
+        Student student1 = new Student();
+        student1.setGender("Männlich");
+        String studentGender = student1.getGender();
+        Assertions.assertEquals("Männlich", studentGender);
+
+    }
+
+}
